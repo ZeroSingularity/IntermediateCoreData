525fbbd218e4b3a98decde5c0fc051b6b21b84c5
Jacobo Hernandez <jacobolhernandez@gmail.com>
2020-09-21 20:40:20 -0500

    Initial Commit

---
+++ IntermediateCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IntermediateCoreData
+//
+//  Created by Jacobo Hernandez on 9/21/20.
+//  Copyright © 2020 Jacobo Hernandez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
